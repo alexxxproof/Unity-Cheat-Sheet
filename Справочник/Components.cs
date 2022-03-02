ee6cb63d1607aba1cea845b7ae7f9b36e447def7
alexxxinter <56309764+alexxxinter@users.noreply.github.com>
2022-03-02 20:39:40 +0300

    Create Components.cs
    Часто используемые компоненты

---
+++ Справочник/Components.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Component c;
+
+    void Start()
+    {
+        //Определяет местоположение объекта в пространстве
+        c = transform;
+        //Определяет форму объекта
+        c = GetComponent<MeshFilter>();
+        //Отрисовывает объект на сцене
+        c = GetComponent<MeshRenderer>();
+        //Отвечает за столкновения
+        c = GetComponent<Collider>();
+        //Придает объекту физические свойства
+        var rb = GetComponent<Rigidbody>();
+        var rbd = rb.drag;          //сопротивление воздуха при движении
+        var rbad = rb.angularDrag;  //сопротивление воздуха при вращении
+        var rbk = rb.isKinematic;   //не действуют столкновения
+    }
+}
